3f9074b1026545365b80309da7012fce84676aea
Srujana <srujanapynni@gmail.com>
2025-03-24 11:19:42 +0530

    Adding a New File

---
+++ script.js
@@ -0,0 +1,9 @@
+// Display an alert when the page loads
+window.onload = function() {
+    alert("Welcome to my page!");
+};
+
+// Change the text of a paragraph when a button is clicked
+function changeText() {
+    document.getElementById("text").innerText = "Text changed!";
+}
